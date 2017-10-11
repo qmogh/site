b714c8009e9a7407f19b5aa1ef2622e6a9180c60
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-11 10:57:51 -0400

    Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,22 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import axios from 'axios'
 
 class Page extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({ pathname, renderPage }) {
     const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />))
     const styleTags = sheet.getStyleElement()
+    // Analytics
+    if (process.env.NODE_ENV !== 'development') {
+      const path = pathname === '/' ? '/index' : pathname
+      axios
+        .get('https://lachlanjc-analytics.glitch.me' + path)
+        .then(res => res.data)
+        .then(json => console.log('Page views:', json.views))
+        .catch(err => console.log('Something went wrong:', err))
+    }
     return { ...page, styleTags }
   }
 

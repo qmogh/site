10e64e81e195a3f577d540d38a43e30504fc39a4
Lachlan Campbell <lachlan@hackclub.com>
2019-09-06 22:05:42 -0400

    Update CSS server rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
-import App, { Container } from 'next/app'
-import { ThemeProvider } from 'theme-ui'
+import App from 'next/app'
+import { ThemeProvider, ColorMode } from 'theme-ui'
 import { Global } from '@emotion/core'
 import Layout from '../components/layout'
 import theme from '../components/theme'
@@ -24,7 +24,7 @@ export default class extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
+      <>
         <Head>
           <title>{name}</title>
           <meta property="twitter:card" content="summary" />
@@ -36,30 +36,33 @@ export default class extends App {
           <meta property="description" content={desc} />
         </Head>
         <ThemeProvider theme={theme}>
-          <Global styles={theme => ({
-            body: {
-              fontFamily: theme.fonts.body,
-              lineHeight: theme.lineHeights.body,
-              fontWeight: theme.fontWeights.body,
-              color: theme.colors.text,
-              backgroundColor: theme.colors.background,
-              margin: 0,
-              minHeight: '100vh',
-              display: 'flex',
-              flexDirection: 'column',
-              '*': {
-                boxSizing: 'border-box'
-              },
-              img: {
-                maxWidth: '100%'
+          <ColorMode />
+          <Global
+            styles={theme => ({
+              body: {
+                fontFamily: theme.fonts.body,
+                lineHeight: theme.lineHeights.body,
+                fontWeight: theme.fontWeights.body,
+                color: theme.colors.text,
+                backgroundColor: theme.colors.background,
+                margin: 0,
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                '*': {
+                  boxSizing: 'border-box'
+                },
+                img: {
+                  maxWidth: '100%'
+                }
               }
-            }
-          })} />
+            })}
+          />
           <Layout>
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
-      </Container>
+      </>
     )
   }
 }

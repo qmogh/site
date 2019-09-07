62cb8a2a37b08e29cda908156ea33d61a5166bff
Lachlan Campbell <lachlan@hackclub.com>
2019-09-06 22:05:08 -0400

    Update social links

--- components/layout.js
+++ components/layout.js
@@ -18,7 +18,7 @@ const ColorButton = ({ mode, ...props }) => (
       m: 0,
       border: 0,
       borderRadius: 9999,
-        lineHeight: 0,
+      lineHeight: 0,
       ':hover, :focus': {
         color: 'primary',
         boxShadow: '0 0 0 3px',
@@ -35,13 +35,7 @@ const ColorButton = ({ mode, ...props }) => (
         stroke="currentcolor"
         strokeWidth="4"
       />
-      <path
-        d={`
-          M 16 0
-          A 16 16 0 0 0 16 32
-          z
-        `}
-      />
+      <path d="M 16 0 A 16 16 0 0 0 16 32 z" />
     </svg>
   </Box>
 )
@@ -51,20 +45,23 @@ export default props => {
 
   return (
     <>
-      <Box sx={{
-  width: '100%',
-  height: '4rem',
-  display: 'block',
-  backgroundImage: theme => `linear-gradient(${theme.colors.red} 0%, ${theme.colors.red} 16.6666%, ${theme.colors.orange} 16.6666%, ${theme.colors.orange} 33.333%, ${theme.colors.yellow} 33.333%, ${theme.colors.yellow} 50%, ${theme.colors.green} 50%, ${theme.colors.green} 66.6666%, ${theme.colors.blue} 66.6666%, ${theme.colors.blue} 83.3333%, #8067C3 83.3333%, #8067C3 100%)`
-
-                }} />
+      <Box
+        sx={{
+          width: '100%',
+          height: '4rem',
+          display: 'block',
+          backgroundImage: theme =>
+            `linear-gradient(${theme.colors.red} 0%, ${theme.colors.red} 16.6666%, ${theme.colors.orange} 16.6666%, ${theme.colors.orange} 33.333%, ${theme.colors.yellow} 33.333%, ${theme.colors.yellow} 50%, ${theme.colors.green} 50%, ${theme.colors.green} 66.6666%, ${theme.colors.blue} 66.6666%, ${theme.colors.blue} 83.3333%, #8067C3 83.3333%, #8067C3 100%)`
+        }}
+      />
       <Flex
         as="header"
         variant="container"
         sx={{
           alignItems: 'center',
           px: 3,
-          py: 4
+          py: 4,
+          'a + a': { mx: [2, 3] }
         }}
       >
         <Avatar size={48} sx={{ mr: 3 }} />
@@ -72,6 +69,16 @@ export default props => {
           <A variant="styles.navitem">@lachlanjc</A>
         </Link>
         <Box sx={{ mx: 'auto' }} />
+        <Link href="/resume">
+          <A variant="styles.navitem">Résumé</A>
+        </Link>
+        <A
+          href="https://notebook.lachlanjc.me"
+          target="_blank"
+          variant="styles.navitem"
+        >
+          Notebook
+        </A>
         <ColorButton
           onClick={e => {
             const next = mode === 'dark' ? 'light' : 'dark'
@@ -105,47 +112,43 @@ export default props => {
         <Flex
           sx={{
             py: 4,
-            justifyContent: 'center'
+            justifyContent: 'center',
+            a: { color: 'primary', mx: 2 }
           }}
         >
-          {/* <Avatar /> */}
-          <A
+          <Avatar sx={{ mr: 3 }} />
+          <a
             href="https://twitter.com/lachlanjc"
             title="Twitter"
+            target="_blank"
             variant="styles.navitem"
-            sx={{ mx: 2 }}
           >
             <Icon glyph="twitter" size={36} />
-          </A>
-          <A
-            href="https://github.com/lachlanjc"
+          </a>
+          <a
+            href="https://github.com/lachlanjc/notebook"
             title="GitHub"
+            target="_blank"
             variant="styles.navitem"
-            sx={{ mx: 2 }}
           >
             <Icon glyph="github" size={36} />
-          </A>
+          </a>
+          <a
+            href="https://instagram.com/@lachlan.jc"
+            title="Instagram"
+            target="_blank"
+            variant="styles.navitem"
+          >
+            <Icon glyph="instagram" size={36} />
+          </a>
+          <a
+            href="mailto:lachlan@hackpenn.com"
+            title="Email"
+            variant="styles.navitem"
+          >
+            <Icon glyph="email" size={36} />
+          </a>
         </Flex>
-        {/*
-        <Flex
-          sx={{
-            flexWrap: 'wrap',
-            alignItems: 'center',
-            fontSize: 0
-          }}
-        >
-          <Link href="/">
-            <A variant="styles.navitem" sx={{ mr: 3 }}>
-              @lachlanjc
-            </A>
-          </Link>
-          <Link href="/about">
-            <A variant="styles.navitem">About</A>
-          </Link>
-          <Box sx={{ mx: 'auto' }} />
-          <Box sx={{ my: 2 }}>© 2019 Lachlan Campbell</Box>
-        </Flex>
-        */}
       </Box>
     </>
   )

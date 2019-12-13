e56b3930441d41b558aa541459639413f12d8827
Lachlan Campbell <lachlan@hackclub.com>
2019-12-13 17:10:55 -0500

    GitHub Web editor strikes again

--- components/blocks.js
+++ components/blocks.js
@@ -56,7 +56,7 @@ export const Tiles = props => (
         m: 0,
         display: 'grid',
         gridGap: [3, 4],
-        gridTemplateColumns: [null, 'repeat(auto-fit, minmax(384px, 1fr))]'
+        gridTemplateColumns: [null, 'repeat(auto-fit, minmax(384px, 1fr))']
       },
       li: {
         bg: 'elevated',

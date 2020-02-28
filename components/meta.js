1bd22664c4b7a112d48c157c23a49d5c82fb67d6
Lachlan Campbell <lachlan@hackclub.com>
2020-02-28 00:55:39 -0500

    Fix head tag location

--- components/meta.js
+++ components/meta.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Head from 'next/head'
 
 const makeTitle = (title, name) =>
   title === name ? title : `${title} – ${name}`
@@ -10,7 +10,7 @@ const Meta = ({
   image = 'https://cdn.glitch.com/4d99d0f7-c364-44a5-b1b9-2c3c3f5cb333%2Fcard.png?v=1582524337857',
   color = '#ff2467'
 }) => (
-  <React.Fragment>
+  <Head>
     <meta key="og_type" property="og:type" content="website" />
     <meta key="og_site" property="og:site_name" content={name} />
     <meta key="og_url" property="og:url" content="https://lachlanjc.me/" />
@@ -43,7 +43,7 @@ const Meta = ({
       key="heap"
       type="text/javascript"
     >{`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};heap.load("2803421074");`}</script>
-  </React.Fragment>
+  </Head>
 )
 
 export default Meta
